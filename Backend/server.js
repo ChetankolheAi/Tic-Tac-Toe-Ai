c5b2140e858305872dbe2dcb9f5c3b3de3f3a3cc
Chetan kolhe <Chetankolhe1020@gmail.com>
2026-04-03 19:48:12 +0530

    ServerChange

--- Backend/server.js
+++ Backend/server.js
@@ -63,7 +63,7 @@ io.on("connection", (socket) => {
 
       // Broadcast new scores to both players
       io.to(roomId).emit("score_updated", rooms[roomId].scores);
-      
+
     }
   });
 
@@ -116,4 +116,8 @@ socket.on("leave_room", (roomId) => {
 });
 });
 
-server.listen(5000, () => console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+  console.log("Server running on port", PORT);
+});
