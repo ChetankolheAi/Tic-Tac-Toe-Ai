e0e3b898ae0ee40060e4d37a9d5d5589cc7fa670
Chetan kolhe <Chetankolhe1020@gmail.com>
2026-04-03 19:53:39 +0530

    ServerChanged

--- Backend/server.js
+++ Backend/server.js
@@ -5,7 +5,9 @@ const cors = require("cors");
 
 const app = express();
 app.use(cors());
-
+app.get("/", (req, res) => {
+  res.send("Server is running 🚀");
+});
 const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: "*" } });
 
