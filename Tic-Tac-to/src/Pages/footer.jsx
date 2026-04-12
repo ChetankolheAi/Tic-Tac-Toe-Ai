2c8a9030aec57a3b33e761a5f898fa1bf8719ef2
Chetan kolhe <Chetankolhe1020@gmail.com>
2026-04-13 00:46:29 +0530

    Footer_Updated

--- Tic-Tac-to/src/Pages/footer.jsx
+++ Tic-Tac-to/src/Pages/footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
   return (
     <div className="w-full h-[50px] bg-slate-900 text-gray-400 flex items-center justify-center text-sm px-4">
-      © 2026 InspiraEye | XO Game 🎮 | All Rights Reserved
+      © 2026 ChetanKolhe | XO Game 🎮 | All Rights Reserved
     </div>
   );
 }
